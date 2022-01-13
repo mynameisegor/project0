0a9bae4384b5d3b16ce72f0b3094560e4b8daf00
Egor <egor.world.91@gmail.com>
2022-01-13 16:22:50 +0500

    practice 1

---
+++ script.js
@@ -0,0 +1,19 @@
+const nomberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: nomberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+};
+
+const a = prompt('один из последних просмотренных фильмов?', ''),
+      b = prompt('на сколько оцените его?', ''),
+      c = prompt('один из последних просмотренных фильмов?', ''),
+      d = prompt('на сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); 

76037476d8df9453c2ecca5af2a4cb374eb1d3cc
Egor <egor.world.91@gmail.com>
2022-01-18 14:33:49 +0500

    practice 1

--- script.js
+++ script.js
@@ -8,12 +8,21 @@ const personalMovieDB = {
     privat: false 
 };
 
-const a = prompt('один из последних просмотренных фильмов?', ''),
-      b = prompt('на сколько оцените его?', ''),
-      c = prompt('один из последних просмотренных фильмов?', ''),
-      d = prompt('на сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('один из последних просмотренных фильмов?', ''),
+          b = prompt('на сколько оцените его?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+
+         
+}
 
 console.log(personalMovieDB); 
